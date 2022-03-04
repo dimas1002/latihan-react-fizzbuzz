a2897977de3eae73a3b8ec9e757a4e464b64d44b
Dimas Pradipto <dimaspradipto11@gmail.com>
2022-03-04 14:45:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1 style={{ color: "red" }}>React FizzBuzz</h1>
+      <p>
+        Counting Incrementlly, Replacing any number that is divisible by three
+        with the word "fizz",any number that is divisible by five with the word
+        "buzz", and any number that is divisible by fifteen with the word
+        "fizzbuzz".
+      </p>
+      <br></br>
+      <hr></hr>
     </div>
   );
+};
+
+class Action extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 1 };
+  }
+
+  handleDecrement = () => {
+    if (this.state.count > 1) {
+      this.setState((prevState) => ({ count: prevState.count - 1 }));
+    }
+  };
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", color: "blue" }}>
+        <h2>
+          {this.state.count % 15 === 0
+            ? "fizzbuzz"
+            : this.state.count % 5 === 0
+            ? "buzz"
+            : this.state.count % 3 === 0
+            ? "fizz"
+            : this.state.count}
+        </h2>
+        <button onClick={this.handleDecrement}>-</button>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+      </div>
+    );
+  }
 }
 
 export default App;
